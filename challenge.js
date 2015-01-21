11073b0a0e600d22f5db8e8d6399cdcdd6e1299e
cassiemak <cassie.mak@gmail.com>
2015-01-21 10:57:25 +0800

    cassie's answer 1

--- challenge.js
+++ challenge.js
@@ -1,10 +1,36 @@
 // CHALLENGE 1
-
 console.log('>>>>>>>>>>CHALLENGE 1');
 
+var array1 = [2, 10, 8, 5];
+var array2 = [4, 25, 7, 13];
 
 
+function sortNumber (a, b){
+  return b - a;
+}
+
+array1 = array1.sort(sortNumber);
+array2 = array2.sort(sortNumber);
+
+var bigDiff = function (sortedArray){
+  console.log(sortedArray);
+  var result = sortedArray[0] - sortedArray[1];
+  console.log(result);
+  return result;
+}
+
+bigDiff(array1);
+bigDiff(array2);
+
+console.log('>>>>>>>>>> END');
 // CHALLENGE 2
+
+var compare = function(array){
+  for (var i = 0;i < array.length; i++){
+    // console.log(array[i]); 
+
+  };
+}
 
 console.log('>>>>>>>>>>CHALLENGE 2');
 
