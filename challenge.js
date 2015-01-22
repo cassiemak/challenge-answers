64964c8db9407dc5c938293cf00fbc5fe24641df
cassiemak <cassie.mak@gmail.com>
2015-01-22 11:06:54 +0800

    i give up

--- challenge.js
+++ challenge.js
@@ -64,14 +64,63 @@ console.log('>>>>>>>>>> END');
 // CHALLENGE 3
 console.log('>>>>>>>>>>CHALLENGE 3');
 
-var sentence = 'My name is Cassandra Mak!';
-console.log(sentence);
+var sentence = 'AABCDbb';
+// console.log(sentence);
+// console.log(sentence[4]);
+// console.log(sentence.length);
+var newWord = [];
 
-var removeDuplicates = function(string){
-
+// every letter would loop through length of letter index -1, if same, do nth, if different, enter in the array
+ 
+var compare = function(string1, string2){
+  if(string2 == string1){
+    return string2;
+  }
+  // else {
+  //   // add string2 to array
+  //   // return string2; 
+  // }
 }
 
 
+var i = 0;
+var j = 1;
+
+
+var removeDuplicates = function(string){
+  // newWord.push(string[0]); 
+    while(i < j) {
+    // console.log('i newWord is ' + newWord);
+
+      for(j = 1; j < string.length; j++){
+
+        if (string[j] == string[i]){
+          console.log(string[j], string[i]);
+          
+          // string.splice(j, 1);
+        }
+        newWord.push(string[j]);
+        console.log('i = ' + i + ' , j = ' + j + ' ' + newWord);
+        i++;
+        // string[compare(string[i], string[j])];
+        // console.log(newWord);
+    }
+  }
+}
+
+        // if(string[i] == string[j]){
+        // console.log('== newWord is ' + newWord);
+        // }
+
+        // else{
+        // newWord.push(string[j]);
+        // console.log(string[j] + ' else push newWord is ' + newWord);
+        // }
+
+removeDuplicates(sentence);
+
+// looop through the entire string.length 
+// from string[0] - string[length]
 
 
 console.log('>>>>>>>>>> END');
